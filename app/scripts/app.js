0a44e7af25b175fea7d7cd2f4bb4f44b7635d985
Justin Merz <jrmerz@gmail.com>
2015-01-08 18:15:52 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+(function(document) {
+  'use strict';
+
+  document.addEventListener('polymer-ready', function() {
+    // Perform some behaviour
+    console.log('Polymer is ready to rock!');
+  });
+
+// wrap document so it plays nice with other libraries
+// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+})(wrap(document));
+
+var SB = {};
---
+++ server.js
@@ -0,0 +1,72 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var auth = require('./lib/auth.js');
+var games = require('./lib/games.js');
+var socket = require('./lib/socket.js');
+var mongo = require('./lib/mongo.js');
+
+
+mongo.connect(function(err) {
+    if( err ) return console.log(err);
+    onConnection();
+});
+
+
+function onConnection() {
+
+    auth.init(app, io, mongo);
+    socket.init(io);
+    games.init(app, mongo, socket);
+
+    app.use(express.static(__dirname + '/app'));
+
+
+    app.get('/rest/players', function(req, resp){
+        var name = req.query.name;
+        if( !name ) return resp.send({error: true, message: 'You must provide a name'});
+
+        name = new RegExp(name, 'i');
+
+        var query = { 
+            $or: [
+                {name : name},
+                {username : name},
+            ],
+            username : { $ne : req.user.username }
+        }
+
+        mongo.queryLimit('users', query, 10,
+            function(err, result) {
+                if( err ) return resp.send({error: true, message: err});
+
+                var arr = [];
+                for( var i = 0; i < result.length; i++ ) {
+                    arr.push(formatSearchResponse(result[i]));
+                }
+
+                resp.send(arr);
+            }
+        );
+    });
+
+    function formatSearchResponse(player) {
+        var resp = {
+            name : player.name,
+            username : player.username,
+            photo : null,
+        }
+
+        if( player.google && player.google.picture ) {
+            resp.photo = player.google.picture;
+        }
+
+        return resp;
+    }
+
+    server.listen(3000);
+}
